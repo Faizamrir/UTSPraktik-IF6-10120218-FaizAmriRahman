30bca38fad4a18bbca7caa85677d610578c200bf
Faizamrir <faizamrirahman9@gmail.com>
2023-05-26 17:05:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if6_10120218_faizamrirahman/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.utspraktik_if6_10120218_faizamrirahman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
